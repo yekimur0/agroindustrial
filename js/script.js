017ecb31bddbd1bdb6b6f53817438afafd974528
yekimur0 <82269937+yekimur0@users.noreply.github.com>
2024-11-26 18:44:54 +1000

    fix header mobile

--- js/script.js
+++ js/script.js
@@ -2,8 +2,14 @@ document.body.addEventListener('click', (e) => {
   let target = e.target;
   if(target.classList.contains('header-a-mobile-products-head')) openProductsMenuMobile();
   if(target.classList.contains('header-a-mobile-products-nav-item')) openProductsSubMenuMobile(target);
+  if (target.classList.contains('header-a-burger')) openMenu(target);
 })
 
+
+function openMenu (target) {
+  target.classList.toggle('active');
+  document.querySelector('.header-a-mobile-content').classList.toggle('active')
+}
 
 function openProductsMenuMobile() {
   document.querySelector('.header-a-mobile-products-nav').classList.toggle('active');
